a5c29c72a693d60d33f9e1af58bc8ba7ac92dbba
Svikol1 <99035768+Svikol1@users.noreply.github.com>
2022-06-22 12:21:50 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,22 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 3) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
     } else {
         basic.showLeds(`
             . . . . .
@@ -23,6 +39,12 @@ radio.onReceivedNumber(function (receivedNumber) {
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     radio.sendNumber(1)
 })
+input.onGesture(Gesture.ScreenDown, function () {
+    radio.sendNumber(3)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(0)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendNumber(2)
 })
